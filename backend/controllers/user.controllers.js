9789b3a826c580aa8086170ef5d3dba99a135906
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2022-09-05 15:24:45 +0200

    feat: users create + hash

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,31 +1,22 @@
 const User = require("../models/Users");
+const bcrypt = require("bcrypt");
 const userValidation = require("../validation/validation");
 
 exports.signup = (req, res) => {
-    // const name = req.body.name;
-    // const email = req.body.email;
-    // const password = req.body.password;
-
-    // Sequelize.query(
-    //     "INSERT INTO users (name, email, password) VALUES (?,?,?)",
-    //     [name, email, password],
-    //     (err, result) => {
-    //         if (err) {
-    //             console.log(err);
-    //         } else {
-    //             res.send("Values Inserted");
-    //         }
-    //     }
-    // );
-
-    // console.log(req.body);
-    const error = userValidation(req);
-    if (error) return res.status(401).json(error);
-    User.create({ ...req.body }).then((result) => {
-        res.status(201).json(result)
-    }).catch((err) => {
-        res.status(500).json(err)
-    });;
-
+    bcrypt
+        .hash(req.body.password, 10)
+        .then((hash) => {
+            const { body } = req;
+            const { error } = userValidation(body);
+            if (error) return res.status(401).json(error.details[0].message);
+            User.create({ ...req.body, password: hash })
+                .then((result) => {
+                    res.status(201).json(result);
+                })
+                .catch((err) => {
+                    res.status(400).json(err);
+                });
+        })
+        .catch((err) => res.status(500).json({ err }));
 };
 exports.login = (req, res) => {};
