d3a7651fcb24e462d26c3f67dc36bcf49b4719dd
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2022-09-12 17:12:41 +0200

    feat: getAllUsers, getOneUser, updateUser

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,7 +1,42 @@
 const UserModel = require("../models/Users");
 
-exports.getAll = (req, res) => {};
-exports.getOne = (req, res) => {};
-exports.createOne = (req, res) => {};
-exports.updateOne = (req, res) => {};
-exports.deleteOne = (req, res) => {};
+exports.getAllUsers = async (req, res) => {
+    const users = UserModel.findAll({
+        attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    })
+        .then((users) => res.status(200).json(users))
+        .catch((err) => res.status(500).json({ err }));
+};
+
+exports.getOneUser = (req, res) => {
+    const { id } = req.params;
+    UserModel.findByPk(id, {
+        attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    })
+        .then((users) => {
+            if (!users)
+                return res
+                    .status(400)
+                    .json({ message: "ID unkown : " + req.params });
+            res.status(200).json(users);
+        })
+        .catch((err) => res.status(500).json({ err }));
+};
+
+exports.updateOneUser = (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    UserModel.findByPk(id)
+        .then((user) => {
+            if (!user)
+                return res
+                    .status(400)
+                    .json({ message: "ID unkown : " + req.params });
+            user.pseudo = body.pseudo
+            user.save().then(
+                res.status(201).json({msg: "User updated !"})
+            ).catch((err) => res.status(500).json({ err }));
+        })
+        .catch((err) => res.status(500).json({ err }));
+};
+exports.deleteOneUser = (req, res) => {};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,9 @@
 const express = require("express");
 // Appel le controller pour associer les fonctions aux différentes routes.
 const authCtrl = require("../controllers/auth.controllers");
-const userCtrl = require('../controllers/user.controllers')
+const userCtrl = require("../controllers/user.controllers");
 // Crée un router en utilisant la méthode .Router() d'express.
 const router = express.Router();
-
 
 /* Route d'authentification */
 
@@ -14,18 +13,16 @@ router.post("/signup", authCtrl.signup);
 // Crée la route post /login avec les fonctions du controller et la méthode .login.
 router.post("/login", authCtrl.login);
 
-/* Route d'utilisateur */ 
+/* Route d'utilisateur */
 
-// Create
-router.post("/", userCtrl.createOne);
 // Read (all)
-router.get("/", userCtrl.getAll);
+router.get("/", userCtrl.getAllUsers);
 // Read (One)
-router.get("/:id", userCtrl.getOne);
+router.get("/:id", userCtrl.getOneUser);
 // Update
-router.put("/:id", userCtrl.updateOne);
+router.put("/:id", userCtrl.updateOneUser);
 // Delete
-router.delete("/:id", userCtrl.deleteOne);
+router.delete("/:id", userCtrl.deleteOneUser);
 
 // Exporte le router pour être accessible par les autres fichiers
 module.exports = router;
