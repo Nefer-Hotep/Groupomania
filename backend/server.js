e7af96b20c8c66339a0d720f83f89a66cdcebddc
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2022-10-22 11:52:50 +0200

    fix : token correction

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
 const app = express();
 
-
-
 // Définie les headers (en-têtes) pour les autorisations CORS.
 app.use((req, res, next) => {
     // Donne le contrôle a tout le monde.
@@ -28,10 +26,10 @@ app.use(express.json());
 
 // routers
 app.use("/api/user", userRoutes);
-app.use('/api/post', postRoutes)
+app.use("/api/post", postRoutes);
 
 // static Images Folder
-app.use('/Images', express.static('./Images'))
+app.use("/images", express.static("./images"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
